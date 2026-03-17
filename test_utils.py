d6d7f43b916d14210ac0eb664ecd82f7b58b1732
Elser Raphael <elserraphael@gmail.com>
2026-03-17 10:39:22 +0100

    add test_utils.py

---
+++ test_utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 10:22:44 2026
+
+@author: raphaelelser
+"""
+from utils import induktiverBlindwiderstand
+from utils import kapzitiverBlindwiderstand
+
+
+def test_induktiverBlindwiderstand():
+    """Test induktiverBlindwiderstand"""
+    assert induktiverBlindwiderstand(0, 0) == 0
+    
+    
+    
+def test_kapzitiverBlindwiderstand():
+    """Test kapzitiverBlindwiderstand"""
+    assert kapzitiverBlindwiderstand(0, 0) == float('inf')
+    
+    
