6c2b939cdf0669a69a85b5f049efe61799b4681e
Elser Raphael <elserraphael@gmail.com>
2026-03-17 10:38:30 +0100

    correct 0 Hz behavior

--- utils.py
+++ utils.py
@@ -18,9 +18,13 @@ def induktiverBlindwiderstand(frequenz, induktivitaet):
 def kapzitiverBlindwiderstand(frequenz, kapazitaet):
     """Kapazitiven Blindwiderstand berechen"""
     Pi = math.pi
-    if (kapazitaet > 0) and (frequenz > 0):
+    if (frequenz > 0) and (kapazitaet > 0):
             creaktanz = 1 / (2 * Pi * abs(frequenz) * abs(kapazitaet))
             return creaktanz 
+    if (frequenz == 0):
+            return float('inf') 
+    if (kapazitaet == 0):
+            return float('inf')    
     
     else: return 0
     
