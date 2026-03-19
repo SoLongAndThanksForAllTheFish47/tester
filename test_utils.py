6330085d0683928159d770a3d236633472fbc835
Elser Raphael <elserraphael@gmail.com>
2026-03-19 19:23:56 +0100

    add a test function for sqrter

--- test_utils.py
+++ test_utils.py
@@ -8,6 +8,7 @@ Created on Tue Mar 17 10:22:44 2026
 
 from utils import induktiverBlindwiderstand
 from utils import kapzitiverBlindwiderstand
+from utils import sqrter
 import math
 
 
@@ -31,3 +32,8 @@ def test_kapzitiverBlindwiderstandZwei():
     """Test kapzitiverBlindwiderstand"""
     Pi = math.pi
     assert kapzitiverBlindwiderstand(1, 1) == 1 / (2 * Pi)
+
+
+def test_sqrter():
+    """Test sqrter"""
+    assert sqrter(3, 4) == 5
