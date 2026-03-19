54dc532ce3947a86476e05285d5996a935e50c7a
Elser Raphael <elserraphael@gmail.com>
2026-03-19 16:11:54 +0100

    add testfunktions

--- test_utils.py
+++ test_utils.py
@@ -8,6 +8,7 @@ Created on Tue Mar 17 10:22:44 2026
 
 from utils import induktiverBlindwiderstand
 from utils import kapzitiverBlindwiderstand
+import math
 
 
 def test_induktiverBlindwiderstand():
@@ -15,6 +16,18 @@ def test_induktiverBlindwiderstand():
     assert induktiverBlindwiderstand(0, 0) == 0
 
 
+def test_induktiverBlindwiderstandZwei():
+    """Test induktiverBlindwiderstand"""
+    Pi = math.pi
+    assert induktiverBlindwiderstand(1, 1) == 2 * Pi
+
+
 def test_kapzitiverBlindwiderstand():
     """Test kapzitiverBlindwiderstand"""
     assert kapzitiverBlindwiderstand(0, 0) == float("inf")
+
+
+def test_kapzitiverBlindwiderstandZwei():
+    """Test kapzitiverBlindwiderstand"""
+    Pi = math.pi
+    assert kapzitiverBlindwiderstand(1, 1) == 1 / (2 * Pi)
