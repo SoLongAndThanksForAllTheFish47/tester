73dd3bb546dd7688102a8b29cb7c60cfce735f86
Elser Raphael <elserraphael@gmail.com>
2026-03-21 09:05:56 +0100

    reformat utils

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ def kapzitiverBlindwiderstand(frequenz, kapazitaet):
     Pi = math.pi
     if (frequenz > 0) and (kapazitaet > 0):
         creaktanz = 1 / (2 * Pi * abs(frequenz) * abs(kapazitaet))
-        return creaktanz 
+        return creaktanz
     else:
         return float("inf")
 
