327843dc42e0e6fd3dd640cacd4314ae84c38828
thibaudpotterie <contact@thibaudpotterie.com>
2026-04-06 12:22:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,17 +14,18 @@ from utils import sqrter
 
 
 def main():
-    """Hauptprogramm"""
+    
 
     print("_Dieses Programm berechnet die Impedanz einer R,L,C Serienschaltung_")
 
-    frequenz = float(input("Frequenz in Hz: "))
+    frequenz =   float(input("Frequenz in Hz: "))
+    
     induktivitaet = float(input("Induktivität in Henry: "))
-    kapazitaet = float(input("Kapazität in Farad: "))
+    kapazitaet =  float(input("Kapazität in Farad: "))
     resistanz = float(input("Resistanz in Ohm: "))
 
     # Impedanz berechen
-    reaktanz = abs(
+    reaktanz =  abs(
         induktiverBlindwiderstand(frequenz, induktivitaet)
         - kapzitiverBlindwiderstand(frequenz, kapazitaet)
     )
